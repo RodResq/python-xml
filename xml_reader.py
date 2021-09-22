f2e90eba823465592df5689cda364890db5d5b04
rodrigo resque <rodrigoresque@yahoo.com.br>
2021-09-22 10:11:35 -0300

    updt

--- xml_reader.py
+++ xml_reader.py
@@ -2,11 +2,15 @@ import xml.etree.ElementTree as ET
 
 tree = ET.parse('country_data.xml')
 root = tree.getroot()
-print(f"A raiz do xml e: {root.tag}")
-print(f"Atributo da tag root: {root.attrib}")
-print(f"Acessando um valor de um no elemento: {root[0][1].text}")
+# print(f"A raiz do xml e: {root.tag}")
+# print(f"Atributo da tag root: {root.attrib}")
+# print(f"Acessando um valor de um no elemento: {root[0][1].text}")
 
-for child in root:
-    print(child.tag, child.attrib)
-    for childreen in child:
-        print(f"Tags dos filhos: tag: {childreen.tag} - atributo(s): {childreen.attrib} - valor: {childreen.text}")
+# for child in root:
+#     print(child.tag, child.attrib)
+#     for childreen in child:
+#         print(f"Tags dos filhos: tag: {childreen.tag} - atributo(s): {childreen.attrib} - valor: {childreen.text}")
+
+# Iterando sobre um elemento especifico
+for neighbor in root.iter('neighbor'):
+    print(f"Atributos da uma tag especifica:  Cidade: {neighbor.attrib.get('name')} - Direcao: {neighbor.attrib.get('direction')}")
