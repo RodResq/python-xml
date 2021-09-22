5d158881b34b61b6c2c8a1a00b37a15d5623a806
rodrigo resque <rodrigoresque@yahoo.com.br>
2021-09-22 09:54:32 -0300

    updt

--- xml_reader.py
+++ xml_reader.py
@@ -4,3 +4,9 @@ tree = ET.parse('country_data.xml')
 root = tree.getroot()
 print(f"A raiz do xml e: {root.tag}")
 print(f"Atributo da tag root: {root.attrib}")
+print(f"Acessando um valor de um no elemento: {root[0][1].text}")
+
+for child in root:
+    print(child.tag, child.attrib)
+    for childreen in child:
+        print(f"Tags dos filhos: tag: {childreen.tag} - atributo(s): {childreen.attrib} - valor: {childreen.text}")
